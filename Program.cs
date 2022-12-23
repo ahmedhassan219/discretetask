97adb77fd5933ec83e7a80cb61aa227f17cc7616
ahmedhassan219 <121302199+ahmedhassan219@users.noreply.github.com>
2022-12-23 17:13:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the number smallest");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the number largest");
+            double n2 = double.Parse(Console.ReadLine());
+
+            for (double i = n1; i <= n2; i++)
+            {
+                double sum = 0;
+                for (double y = 1; y < i; y++)
+                {
+                    if (i % y == 0)
+                        sum += y;
+                }
+                if (sum == i)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
